406340c83d3712d8b777b0c495f9a4623ccf471f
Christopher Patton <cjpatton@ucdavis.edu>
2013-06-04 13:01:34 -0700

    added sample term

--- init.sql
+++ init.sql
@@ -4,8 +4,11 @@ create table if not exists term
 (
   id integer not null, 
   termString text not null, 
-  definition text not null, 
-  examples text not null, 
+  definition text not null,
+  --examples text not null, 
+  --relations (?)
+  --status enum {vernacular, canonical, archival}
+  --contactInfo 
   score integer not null,
   created timestamp not null, 
   modified timestamp not null, 
